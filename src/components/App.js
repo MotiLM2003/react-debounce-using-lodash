33a6f58e27e7f43bdbad656cd67de26640381c28
Moti Elmakies <motiphone2003@gmail.com>
2020-11-23 10:31:55 +0200

    :astronaut: (initial commit) - good luck

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+import debounce from 'lodash.debounce';
+
+import './App.css';
+
+function App() {
+  const [value, setValue] = useState('');
+  const [dbValue, saveToDb] = useState(''); // would be an API call normally
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <main>
+      <h1>Blog</h1>
+      <textarea value={value} onChange={handleChange} rows={5} cols={50} />
+      <section className='panels'>
+        <div>
+          <h2>Editor (Client)</h2>
+          {value}
+        </div>
+        <div>
+          <h2>Saved (DB)</h2>
+          {dbValue}
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
